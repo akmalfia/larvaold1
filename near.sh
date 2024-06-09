859061db83b4ecc693e815706357679051a69e25
akmalfia <158145860+akmalfia@users.noreply.github.com>
2024-06-09 01:45:41 -0700

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://near.lava.build/lava-referer-be8f1571-9888-4bcd-b337-1d89cb4c2984/"
 
 OUTPUT_DIR="logs"
 
